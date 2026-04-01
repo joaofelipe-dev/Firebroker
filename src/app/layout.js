ec0df8b954e92cf5f9ce8f2848793412979f8f61
João Felipe <joaoufelipe@hotmail.com>
2026-04-01 19:27:45 -0300

    Alterado para tema escuro

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body className="dark">{children}</body>
     </html>
   );
 }
